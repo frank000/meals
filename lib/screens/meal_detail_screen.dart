6aaf19b9c038f24d78022087c67af8a402c18c5e
franklim.paulino <franklim.ti@gmail.com>
2022-11-06 16:14:37 -0300

    Meloria tela detalhe

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -12,7 +12,40 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Center(child: Text('Detalhes da refeição ')),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 300,
+            child: Image.network(meal.imageUrl, fit: BoxFit.cover),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            child: Text('Ingredientes',
+                style: Theme.of(context).textTheme.bodyMedium),
+          ),
+          Container(
+            width: 200,
+            height: 220,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(10)),
+            child: ListView.builder(
+                itemCount: meal.ingredients.length,
+                itemBuilder: (ctx, i) {
+                  return Card(
+                      child: Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                          child: Text(meal.ingredients[i])),
+                      color: Theme.of(context).colorScheme.secondary);
+                }),
+          ),
+        ],
+      ),
     );
   }
 }
