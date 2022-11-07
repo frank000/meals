0ea937177c2affc5ac9d622aeaf7b0500d53fc50
franklim.paulino <franklim.ti@gmail.com>
2022-11-06 21:17:13 -0300

    Implementação do Drawer

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/main_drawer.dart';
 import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/favorite_screen.dart';
 
@@ -31,6 +32,7 @@ class _TabsScreenState extends State<TabsScreen> {
         ),
       ),
       body: _screens[_selectedScreen]['screen'] as Widget,
+      drawer: MainDrawer(),
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectScreen,
         backgroundColor: Theme.of(context).colorScheme.primary,
